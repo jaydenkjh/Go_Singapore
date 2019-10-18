fba7b7eca759b3b8fe589d3ddf986c98308cb300
NI0001JI <44900283+NI0001JI@users.noreply.github.com>
2019-10-18 20:54:43 +0800

    Added a new control class: DBManager
    DBManager with 4 basic methods:
    Validate() -> return boolean value based on whether username - password combination exist in the table
    InsertEntry() -> self explanatory
    DeleteEntry() -> self explanatory
    UpdatePassword() -> changes the password of a given email entry

---
+++ Go_Singapore/Controllers/DBManager.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Go_Singapore.Controllers
+{
+    public class DBManager // class containing all the methods to interact with the database
+    {
+        // change the connection string whenever the database's directory is moved
+        static string connectionString = "Data Source="+@"(LocalDB)\MSSQLLocalDB;AttachDbFilename="+@"|INSERT DATA DIRECTORY HERE|GoSingaporeDB.mdf"+";Integrated Security=True";
+        // Validate() takes in the email and password combination of the user, if they exist in the users table, the method will return true, else false
+        public static bool Validate(string inputEmail, string inputPassword)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand command;
+            string validateCommand = "SELECT COUNT(1) FROM users WHERE email = '" + inputEmail + "' AND password = '" + inputPassword + "'";
+            con.Open();
+
+            try
+            {
+                command = new SqlCommand(validateCommand, con);
+                int result = Convert.ToInt32(command.ExecuteScalar());
+                if (result == 1)
+                {
+                    command.Dispose();
+                    con.Close();
+                    return true;
+                }                    
+                else
+                {
+                    command.Dispose();
+                    con.Close();
+                    return false;
+                }
+                    
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("WARNING: " + e.ToString());
+                Console.WriteLine("Please try again ... ");
+            }
+            
+            con.Close();
+            return false;            
+        }
+        
+        // InsertEntry() takes in 4 string arguments and insert them into the "users" table
+        public static void InsertEntry(string inputName, string inputUserName, string inputEmail, string inputPassword)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlDataAdapter da = new SqlDataAdapter();
+            SqlCommand command;
+            string insertCommand = "";
+            con.Open();
+
+            try
+            {
+                insertCommand = "INSERT INTO Users (name, username, password, email) " +
+                    "VALUES '" + inputName + "','" + inputUserName + "','" + inputEmail + "','" + inputPassword + "')";
+                command = new SqlCommand(insertCommand, con);
+                da.InsertCommand = command;
+                da.InsertCommand.ExecuteNonQuery();
+                Console.WriteLine("Item successfully inserted");
+                da.Dispose();
+                command.Dispose();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("WARNING: " + e.ToString());
+                Console.WriteLine("Please try again ... ");
+            }
+
+            con.Close();
+        }
+
+        public static void DeleteEntry(string inputEmail)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlDataAdapter da = new SqlDataAdapter();
+            SqlCommand command;
+            string deleteCommand = "";
+            con.Open();
+
+            try
+            {
+                deleteCommand = "DELETE FROM users WHERE email = '" + inputEmail + "'";
+                command = new SqlCommand(deleteCommand, con);
+                da.DeleteCommand = command;
+                da.DeleteCommand.ExecuteNonQuery();
+                Console.WriteLine("Item successfully deleted");
+                da.Dispose();
+                command.Dispose();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("WARNING: " + e.ToString());
+                Console.WriteLine("Please try again ... ");
+            }
+
+            con.Close();
+        }
+
+        // UpdatePassword() takes in the requester's email and updates its password to a new one
+        // We may want to combine Validate() with UpdatePassword
+        public static void UpdatePassword(string inputEmail, string inputPassword)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlDataAdapter da = new SqlDataAdapter();
+            SqlCommand command;
+            string updateCommand = "";
+            con.Open();
+
+            try
+            {
+                updateCommand = "UPDATE users SET password = '" + inputPassword + "' WHERE email = '" + inputEmail + "'";
+                command = new SqlCommand(updateCommand, con);
+                da.UpdateCommand = command;
+                da.UpdateCommand.ExecuteNonQuery();
+                Console.WriteLine("Password successfully updated");
+                da.Dispose();
+                command.Dispose();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("WARNING: " + e.ToString());
+                Console.WriteLine("Please try again ... ");
+            }
+
+            con.Close();
+        }
+        
+        
+    }
+}
