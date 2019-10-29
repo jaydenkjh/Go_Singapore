9777ce58078aabdbb06d59d7ca2f339566392c6b
NI0001JI <44900283+NI0001JI@users.noreply.github.com>
2019-10-29 19:33:46 +0800

    LoginProcess() Method Added
    LoginProcess() takes in two parameters, the string of username and password. Outputs "1" when successful, otherwise "invalid credentials"
    
    The method is used by LoginPage.aspx.cs

--- Go_Singapore/Controllers/LoginManager.cs
+++ Go_Singapore/Controllers/LoginManager.cs
@@ -7,5 +7,43 @@ namespace Go_Singapore.Controllers
 {
     public class LoginManager
     {
+        public static String LoginProcess(String strUsername, String strPassword)
+        {
+            String message = "";
+            //my connection string
+            SqlConnection con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\GoSingaporeDB.mdf;Integrated Security = True;");
+            SqlCommand cmd = new SqlCommand("Select * from Users where username=@Username", con);
+            cmd.Parameters.AddWithValue("@Username", strUsername);
+            try
+            {
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    Boolean login = (strPassword.Equals(reader["password"].ToString(), StringComparison.InvariantCulture)) ? true : false;
+                    if (login)
+                    {
+                        message = "1";
+                        
+
+                    }
+                    else
+                        message = "Invalid Credentials";
+                }
+                else
+                    message = "Invalid Credentials";
+
+                reader.Close();
+                reader.Dispose();
+                cmd.Dispose();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message.ToString() + "Error.";
+
+            }
+            return message;
+        }
     }
 }
